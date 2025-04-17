b5d6733fcfb7252becfa091f0e84dbbbb8028e6a
Camila Medrano <c.medranoprado0105@gmail.com>
2025-04-17 21:01:02 +0200

    Update constellations.py for 3d visualization.

--- constellations/constellations.py
+++ constellations/constellations.py
@@ -2,9 +2,15 @@ from constellations.constellations_parser import read_constellations
 
 class Constellation():
     """
-    A constellation defined by a sequence of star HR numbers.
+    Represents a constellation defined by a sequence of star HR numbers,
+    with associated Star instances holding 3D homogeneous coordinates.
+
+    Attributes:
+        name (str): Name of the constellation.
+        hr_sequence (List[int]): Sequence of HR numbers defining star order.
+        stars (List[Star]): Bound Star objects (with .homogeneous attributes).  
     """
-    def __init__(self, name, hr_sequence):
+    def __init__(self, name: str, hr_sequence: list):
         self.name = name
         self.hr_sequence = hr_sequence
         self.stars = []
@@ -12,28 +18,27 @@ class Constellation():
     def __repr__(self):
         return (f"Constellation {self.name}: ({self.hr_sequence})")
 
-    def bind_stars(self, star_lookup):
+    def bind_stars(self, star_lookup: dict):
         """
-        Populate self.stars with Star objects from the lookup dict.
+        Populate "stars" list with Star instances from the lookup dict.
 
         Paramenters:
-            star_lookup (dict): mappping HR -> Star instance
+            star_lookup (dict): Mappping HR (int) -> Star instance
         """
         self.stars = []
         for hr in self.hr_sequence:
             star = star_lookup.get(hr)
-            if star:
+            if star is not None:
                 self.stars.append(star)
         # After this, self.stars contains the Star objects in the same order as hr_sequence
 
 
 def load_constellations(star_lookup):
     """"
-    Read raw constellations data and return list of Constallion instances
-    with their "stars" list bound to Star objects.
+    Read raw constellation definitions and bind Star objects.
 
     Parameters:
-        star_lookup (dict): dict mapping HR (int) to Star
+        star_lookup (dict): Mapping HR (int) -> Star instance
 
     Return:
         constellations (list): list of Constellation instances.
